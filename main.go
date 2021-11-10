a28a61975c99f2ba414a7c97a1af0a097cd95948
Furkan Akyol <furkanakyol@Furkans-MacBook-Air.local>
2021-11-10 16:55:39 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/json"
+)
+
+type IHeaderSerializer interface {
+	encodePageHeader(pageHeader PageHeader) []byte
+	readPageHader(page []byte) PageHeader
+}
+
+// binary serializer implementation
+type binarySerializer struct{}
+
+func (r binarySerializer) readPageHader(page []byte) PageHeader {
+	header := page[:pageHeaderSize]
+
+	reader := bytes.NewReader(header)
+
+	pageHeader := PageHeader{}
+	binary.Read(reader, binary.LittleEndian, &pageHeader)
+
+	return pageHeader
+}
+
+func (r binarySerializer) encodePageHeader(pageHeader PageHeader) []byte {
+	buf := bytes.Buffer{}
+	err := binary.Write(&buf, binary.LittleEndian, pageHeader)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	b := buf.Bytes()
+	return b
+}
+
+// json serializer implementation
+type jsonSerializer struct{}
+
+func (r jsonSerializer) readPageHader(page []byte) PageHeader {
+	var res PageHeader
+	header := page[:pageHeaderSize]
+	json.Unmarshal(header, &res)
+	println(res.IsUsed)
+	return res
+}
+
+func (r jsonSerializer) encodePageHeader(pageHeader PageHeader) []byte {
+	b, _ := json.Marshal(pageHeader)
+	return b
+}
