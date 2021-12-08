6434e34b4443dee61d9cca5a589cc9a1d62bef51
thetarby <furkanakyol1997@gmail.com>
2021-12-09 02:32:36 +0300

    added new tests

--- execution/expressions/comparison_expression.go
+++ execution/expressions/comparison_expression.go
@@ -18,15 +18,19 @@ const (
 
 type CompExpression struct{
 	BaseExpression
-	compType CompType
+	CompType CompType
 }
 
 func (e *CompExpression) Eval(t catalog.Tuple, s catalog.Schema) db_types.Value{
 	lhs := e.GetChildAt(0).Eval(t, s)
 	rhs := e.GetChildAt(1).Eval(t, s)
-	res := doComparison(e.compType, lhs, rhs)
+	res := doComparison(e.CompType, lhs, rhs)
 
 	return *db_types.NewValue(res)
+}
+
+func (e *CompExpression) GetCompType(t catalog.Tuple, s catalog.Schema) CompType{
+	return e.CompType
 }
 
 func doComparison(compType CompType, lhs, rhs db_types.Value) bool{
@@ -38,7 +42,31 @@ func doComparison(compType CompType, lhs, rhs db_types.Value) bool{
 			return false
 		}
 		return true
+	case NotEqual:
+		if lhs.Less(&rhs){
+			return true
+		}else if rhs.Less(&lhs){
+			return true
+		}
+		return false
+	case LessThan:
+		return lhs.Less(&rhs)
+	case LessThanOrEqual:
+		return lhs.Less(&rhs) || !rhs.Less(&lhs)
+	case GreaterThan:
+		return rhs.Less(&lhs)
+	case GreaterThanOrEqual:
+		return rhs.Less(&rhs) || !lhs.Less(&rhs)
 	default:
 		panic("implement me")
+	}
+}
+
+func NewCompExpr(cType CompType, lhs, rhs IExpression) *CompExpression{
+	return &CompExpression{
+		BaseExpression: BaseExpression{
+			Children: []IExpression{lhs, rhs},
+		},
+		CompType:       cType,
 	}
 }
